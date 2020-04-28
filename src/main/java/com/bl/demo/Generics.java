fe2f2969001e83f21b94ba4966c60a6fa3272e66
250399 <kunaldeshmukh2503@gmail.com>
2020-04-27 20:08:53 -0700

    ADDED: Generic method for displaying arrays

--- src/main/java/com/bl/demo/Generics.java
+++ src/main/java/com/bl/demo/Generics.java
@@ -2,12 +2,15 @@ package com.bl.demo;
 
 public class Generics {
     static public void main(String[] args) {
-        int[] intArray =  {1,2,3,4,5,6,7,8};
+        Integer[] intArray =  {1,2,3,4,5,6,7,8};
         String[] stringArray = {"one","Tow","Three","Four","Five"};
         Double[] doubleArray = {1.2,33.1,44.2,55.4};
         toPrint(intArray);
         toPrint(stringArray);
         toPrint(doubleArray);
+        toPrint_UsingGenerics(stringArray);
+        toPrint_UsingGenerics(intArray);
+        toPrint_UsingGenerics(doubleArray);
     }
 
     static void toPrint(String[] arr){
@@ -17,7 +20,7 @@ public class Generics {
         }
     }
 
-    static void toPrint(int[] arr){
+    static void toPrint(Integer[] arr){
         System.out.println("Int array elements:");
         for(Integer x : arr){
             System.out.println(x);
@@ -27,6 +30,13 @@ public class Generics {
     static void toPrint(Double[] arr){
         System.out.println("Double array elements:");
         for(Double x : arr){
+            System.out.println(x);
+        }
+    }
+
+    static <T> void toPrint_UsingGenerics(T[] arr){
+        System.out.println("Printing Using Generics");
+        for(T x : arr){
             System.out.println(x);
         }
     }
