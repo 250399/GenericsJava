a64e6ebf0e4ad1a07d23b7542a278612736b08b4
250399 <kunaldeshmukh2503@gmail.com>
2020-04-27 19:52:43 -0700

    ADDED:Printing array of String, Integer, Double

--- src/main/java/com/bl/demo/Generics.java
+++ src/main/java/com/bl/demo/Generics.java
@@ -1,4 +1,33 @@
 package com.bl.demo;
 
 public class Generics {
+    static public void main(String[] args) {
+        int[] intArray =  {1,2,3,4,5,6,7,8};
+        String[] stringArray = {"one","Tow","Three","Four","Five"};
+        Double[] doubleArray = {1.2,33.1,44.2,55.4};
+        toPrint(intArray);
+        toPrint(stringArray);
+        toPrint(doubleArray);
+    }
+
+    static void toPrint(String[] arr){
+        System.out.println("String array elements:");
+        for(String x : arr){
+            System.out.println(x);
+        }
+    }
+
+    static void toPrint(int[] arr){
+        System.out.println("Int array elements:");
+        for(Integer x : arr){
+            System.out.println(x);
+        }
+    }
+
+    static void toPrint(Double[] arr){
+        System.out.println("Double array elements:");
+        for(Double x : arr){
+            System.out.println(x);
+        }
+    }
 }
